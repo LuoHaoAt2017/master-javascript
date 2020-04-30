351761ed2468ce5691a9fb3a2c0786e83bd64ff9
LuoHao-RenRen <hluo5206@gmail.com>
2020-04-30 21:54:31 +0800

    feat: 采用首节点和不采用首节点合并两个链表。

--- src/cross-origin/index.js
+++ src/cross-origin/index.js
@@ -1,6 +1,7 @@
-
 function ajax(config, cb) {
     const xhr = createXHR();
+    // 前端设置是否带cookie
+    xhr.withCredentials = true;
      // step0: 检测状态变化
     xhr.onreadystatechange = function() {
         // statechange=0, 未初始化
@@ -8,7 +9,10 @@ function ajax(config, cb) {
         // statechange=2，发送，已经启动send方法。
         // statechange=3，开始接收到服务器响应。
         // statechange=4，接收到全部响应数据。
-        if (xhr.readyState === 2) {
+        if (xhr.readyState === 1) {
+            xhr.setRequestHeader('Content-Type', 'application/json');
+            xhr.setRequestHeader('origin', 'http://test-api.truckerpath.com');
+        } else if (xhr.readyState === 2) {
             setTimeout(() => {
                 xhr.abort();
             }, config.timeout);
@@ -31,14 +35,18 @@ function createXHR() {
         return new ActiveXObject('Microsoft.XMLHTTP');
     }
 }
-const baidu = 'http://test-api.truckerpath.com//jobs-v2-api/v1/third/party/search/record';
-const nczonline = 'http://www.nczonline.net';
-window.onload = function () {
+
+const truckerpath = 'http://test-api.truckerpath.com//jobs-v2-api/v1/third/party/search/record';
+const jiumo = 'https://www.jiumodiary.com/images/wechat_public_barcode3.gif';
+
+function loadtruckerpath() {
 	ajax({
 		type: 'get',
-		url: baidu, 
+		url: truckerpath, 
 		async: true,
-        timeout: 3000
+        timeout: 3000,
+        header: {},
+        // data: "user=admin"
 	}, function(resp) {
         const app = document.getElementById("app");
         resp = JSON.parse(resp);
@@ -46,12 +54,32 @@ window.onload = function () {
         const list = document.createElement('ul');
         // 优化DOM操作：减少DOM访问。
         const fragment = document.createDocumentFragment();
-        locations.forEach(function(elem) {
+        locations && locations.forEach(function(elem) {
             const item = document.createElement('li');
             item.innerText = elem.state;
             fragment.appendChild(item);
         });
         list.appendChild(fragment);
-        app.appendChild(list);
+		app.appendChild(list);
+		
 	});
+}
+
+function useImg() {
+    // jsonp方式实现跨域,只能够实现get请求。
+    // 通过标签去实现get请求的跨域。不支持其它的方法。
+    const img = document.createElement('img');
+    img.src = jiumo;
+    document.body.appendChild(img);
+}
+
+function useScript() {
+    // 接口成功返回结果但是浏览器无法读取
+    const script = document.createElement('script');
+    script.src = truckerpath;
+    document.body.appendChild(script);
+}
+
+window.onload = function () {
+    loadtruckerpath();
 };
--- src/xml-http-request/index.js
+++ src/xml-http-request/index.js
@@ -1,11 +1,58 @@
+function ajax(config, cb) {
+    const xhr = createXHR();
+     // step0: 检测状态变化
+    xhr.onreadystatechange = function() {
+        // statechange=0, 未初始化
+        // statechange=1，启动，已经启动open方法。
+        // statechange=2，发送，已经启动send方法。
+        // statechange=3，开始接收到服务器响应。
+        // statechange=4，接收到全部响应数据。
+        if (xhr.statechange === 2) {
+            setTimeout(() => {
+                xhr.abort();
+            }, config.timeout);
+        } else if (xhr.statechange === 4) {
+			debugger;
+            if (xhr.status === 200) {
+                cb(xhr.responseText);
+            } else {
+                console.error('status: ', xhr.status);
+            }
+        }
+    }
+    xhr.open(config.type, config.url, config.async); // step1: 准备请求 params: method, url, async
+    xhr.send(null); // step2: 发送请求
+}
+
+function createXHR() {
+    if (window.XMLHttpRequest !== undefined) {
+        return new XMLHttpRequest();
+    } else {
+        return new ActiveXObject('Microsoft.XMLHTTP');
+    }
+}
+
 window.onload = function () {
-	Ajax({
-		type: 'get', 
-		url: 'https://www.baidu.com', 
+	const truckerpath = 'http://test-api.truckerpath.com//jobs-v2-api/v1/third/party/search/record';
+	ajax({
+		type: 'get',
+		url: truckerpath, 
 		async: true,
-		timeout: 3000
+        timeout: 3000
 	}, function(resp) {
-		const app = document.getElementById("app");
-		app.innerText = resp;
+        const app = document.getElementById("app");
+        resp = JSON.parse(resp);
+        const locations = resp.data.hotLocation;
+        const list = document.createElement('ul');
+        // 优化DOM操作：减少DOM访问。
+        const fragment = document.createDocumentFragment();
+        locations.forEach(function(elem) {
+            const item = document.createElement('li');
+            item.innerText = elem.state;
+            fragment.appendChild(item);
+        });
+        list.appendChild(fragment);
+		app.appendChild(list);
+		
 	});
 };
