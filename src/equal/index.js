ede0bc927a4710530e3b8e01bac610f88db27fbc
LuoHao-RenRen <hluo5206@gmail.com>
2020-05-07 11:30:53 +0800

    fix: reset

--- src/equal/index.js
+++
@@ -1,7 +0,0 @@
-
-const obj = new Object();
-let obj1 = obj;
-let obj2 = obj;
-// 两个引用是否指向同一块内存
-console.log('obj1 === obj2: ', obj1 === obj2);
-console.log('obj1 === obj2: ', Object.is(obj1, obj2));
