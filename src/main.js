39f5f4190803b708e03d6224506dacd2a50d68b5
LuoHao-RenRen <hluo5206@gmail.com>
2020-05-29 09:33:02 +0800

    style: nav

---
+++ src/main.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './views/App.js';
+import "@/assets/scss/app.scss";
+import "@/assets/scss/reset.scss";
+import "@/assets/scss/common.scss";
+ReactDOM.render(<App/>, document.getElementById('root'));
---
+++ src/views/prototype.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Prototype() {
+    return (
+        <div className='container'>
+            Hello JavaScript
+        </div>
+    )
+}
+
+export default Prototype;
