7af779ca9c5f26c7eb38b00d42a5af7ae8c0c44f
LuoHao-RenRen <hluo5206@gmail.com>
2020-06-10 20:54:04 +0800

    feat: nothing

--- src/views/chapter1/test.js
+++ src/views/chapter1/test.js
@@ -1,11 +1,19 @@
 function A() {
 
 }
+
 function B() {
 
 }
+
 const a = new A();
-console.log(a.constructor);
 a.__proto__ = B.prototype;
+
+// typeof 
+console.log(typeof a);
+// instanceof 
 console.log(a instanceof B);
+// constructor 
 console.log(a.constructor);
+// toString 
+console.log(Object.prototype.toString(a));
